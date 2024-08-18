8a1f1c841cde395f4a7a03d6049d85d4fc0b570b
RoloDMonkey <rolo@irolo.net>
2024-08-18 19:50:09 -0400

    - remove jump from gnome - add bounce when colliding with item group (mushrooms) - add new collision layers: player, items - added basic mushroom scene - add new item group to project - added mushroom images - set up collision layers and masks for tilemap, gnome, mushroom - added basic mushrooms to level one, changed tilemap so that gnome can get through level

--- gnome_tiny.gd
+++ gnome_tiny.gd
@@ -2,17 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+# Vertical impulse applied to the character upon bouncing on an item.
+@export var bounce_impulse = 400
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,5 +19,22 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
 
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with an item.
+		if collision.get_collider().is_in_group("item"):
+			# we check that we are hitting it from above.
+			if Vector2.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				velocity.y = -1 * bounce_impulse
+				break
+				
 	move_and_slide()
